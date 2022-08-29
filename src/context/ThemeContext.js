c46090925f13839c1ff24bfa848ea4e58090c743
enriquejgilq <35854635+enriquejgilq@users.noreply.github.com>
2022-08-29 10:25:15 -0500

    practice

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,21 +1,21 @@
-import React, {createContext, useState} from 'react';
+import React, { createContext, useState } from 'react';
 //const ThemeContext =  createContext( )
 //export default ThemeContext
 
 const DarkModeContext = createContext();
 
-function DarkModeProvider(props){
+function DarkModeProvider(props) {
     const [darkMode, setDarkMode] = useState(false);
     const toggleDarkMode = (dark) => {
         setDarkMode(!darkMode);
     };
     return (
-        <div>  
-            <DarkModeContext.Provider value={{darkMode, toggleDarkMode}}>
+        <div>
+            <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
                 {props.children}
             </DarkModeContext.Provider>
         </div>
     )
 };
 
-export {DarkModeContext, DarkModeProvider};
+export { DarkModeContext, DarkModeProvider };
